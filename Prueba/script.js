4a65fbab1714de7b6561bff69306dbafb5d88801
carlosltn0203 <145719493+carlosltn0203@users.noreply.github.com>
2024-05-27 20:12:21 -0500

    Delete Prueba directory

--- Prueba/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $("#fecha").datepicker();
-    $("#checkdisponibilidad").on("click", function(){
-        var selectedDate = $("#fecha").val();
-        $getJSON(https://github.com/carlosltn0203/casera.ec/blob/e02cb3de85ca67004f31c24ec6aa9791c0002c0e/Prueba/disponibilidad.json,function(data){
-            console.log(data);
-        });
-    });
-});
