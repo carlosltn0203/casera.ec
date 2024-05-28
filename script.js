b8278d766632ed2698168be756a36063bb58dd28
carlosltn0203 <145719493+carlosltn0203@users.noreply.github.com>
2024-05-27 20:23:41 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $("#fecha").datepicker();
     $("#checkdisponibilidad").on("click", function(){
         var selectedDate = $("#fecha").val();
-        $getJSON(,function(data){
+        $getJSON(https://github.com/carlosltn0203/casera.ec/blob/929ccac84429c7951eb28df1060ad2b0bd889cc6/disponibilidad.json,function(data){
             console.log(data);
         });
     });
