9dc0743317fb493fc27bb0a9a96bfe1b26de12e7
carlosltn0203 <145719493+carlosltn0203@users.noreply.github.com>
2024-05-27 20:19:02 -0500

    Delete Prueba directory

--- Prueba/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $("#fecha").datepicker();
-    $("#checkdisponibilidad").on("click", function(){
-        var selectedDate = $("#fecha").val();
-        $getJSON(,function(data){
-            console.log(data);
-        });
-    });
-});
